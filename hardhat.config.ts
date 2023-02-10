7244db1ac958ed93056bff2d9308948da88a6432
Sam Goldman <sam@quixotic.io>
2023-02-09 21:45:56 -0500

    setup chugsplash project

---
+++ hardhat.config.ts
@@ -0,0 +1,23 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import '@chugsplash/plugins'
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.15',
+        settings: {
+          outputSelection: {
+            '*': {
+              '*': ['storageLayout'],
+            },
+          },
+        },
+      },
+    ]
+  }
+}
+
+export default config
