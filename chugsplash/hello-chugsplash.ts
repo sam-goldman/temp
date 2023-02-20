92a6000872c3875bc93047c0177f5a8506cf092e
Sam Goldman <sam@quixotic.io>
2023-02-19 23:18:20 -0500

    test

--- chugsplash/hello-chugsplash.ts
+++ chugsplash/hello-chugsplash.ts
@@ -2,7 +2,7 @@ import { UserChugSplashConfig } from '@chugsplash/core'
 
 const config: UserChugSplashConfig = {
   options: {
-    projectName: 'Hello ChugSplash',
+    projectName: "Sam's project",
   },
   contracts: {
     MyFirstContract: {
